d128e84a1573d91fd5f5de87f29cfe5923a59470
Rajendra Pun <rajendrapun20@gmail.com>
2022-05-27 09:12:54 +0545

    settings

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # for collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
